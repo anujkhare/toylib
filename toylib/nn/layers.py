e8178de73f772e2d09abbe5cef2596975456ee32
Anuj Khare <khareanuj18@gmail.com>
2025-04-13 22:43:56 -0400

    Initial working toylib Module and mlp

--- toylib/nn/layers.py
+++ toylib/nn/layers.py
@@ -1,27 +1,31 @@
 import math
-from typing import Optional
-
-from jaxtyping import Array, PRNGKeyArray
-
 import jax
+import jaxtyping
+import typing
 
-from . import module
+from toylib.nn import module
 
 
+@jax.tree_util.register_pytree_node_class
 class Linear(module.Module):
-    """Defines a simple feedforward layer: which is a linear transformation."""
+    """Defines a simple feedforward layer: which is a linear transformation. """
 
-    weights: Array
-    bias: Optional[Array]
+    # Trainable parameters
+    weights: jaxtyping.Array
+    bias: typing.Optional[jaxtyping.Array]
 
+    # Hyperparameters / metadata
     in_features: int
     out_features: int
     use_bias: bool
 
-    def __init__(self, in_features: int, out_features: int, use_bias: bool = True, *, key: PRNGKeyArray) -> None:
+    def __init__(self, in_features: int, out_features: int, use_bias: bool = True, *, key: jaxtyping.PRNGKeyArray) -> None:
+        # Split the random key for weights and bias
         w_key, b_key = jax.random.split(key, 2)
+        
+        # We initialize the weights with a uniform distribution
         lim = 1 / math.sqrt(in_features)
-        self.weights = jax.random.uniform(w_key, (out_features, in_features), minval=-lim, maxval=lim)
+        self.weights = jax.random.uniform(w_key, (in_features, out_features), minval=-lim, maxval=lim)
         if use_bias:
             self.bias = jax.random.uniform(b_key, (out_features,), minval=-lim, maxval=lim)
 
@@ -30,11 +34,8 @@ class Linear(module.Module):
         self.use_bias = use_bias
         self.key = key
 
-    def __call__(self, x: Array) -> Array:
-        x = jax.numpy.dot(self.weights, x)
+    def __call__(self, x: jaxtyping.Array) -> jaxtyping.Array:
+        x = jax.numpy.dot(x, self.weights)
         if self.use_bias:
             x = x + self.bias
         return x
-
-
-module.register_toylib_module(Linear)
--- toylib/nn/module.py
+++ toylib/nn/module.py
@@ -1,22 +1,25 @@
-from abc import ABC
 from typing import Any
-
 import numpy as np
-
 import jax
-from jax.tree_util import register_pytree_with_keys
+import typing
 
 
 def _is_array(x: Any) -> bool:
-    return isinstance(
-        x, (jax.Array, np.ndarray, np.generic)
-    ) or hasattr(x, "__jax_array__")
+    return isinstance(x, (jax.Array, np.ndarray, np.generic)) or hasattr(
+        x, "__jax_array__"
+    )
 
 
 def _is_random_key(x: str) -> bool:
-    return x == 'key'
+    return x == "key"
 
-class Module(ABC):
+
+def _is_supported_container(x: typing.Any) -> bool:
+    return isinstance(x, (list, tuple))
+
+
+@jax.tree_util.register_pytree_node_class
+class Module:
     """
     Defines a base class to use for the neural network modules in toylib.
 
@@ -28,26 +31,45 @@ class Module(ABC):
 
     Inspired by equinox and the Custom PyTres and Initialization section in jax docs.
     """
-    def tree_flatten(self):
-        dynamic = [(k, v) for k, v in self.__dict__.items() if _is_array(v) and not _is_random_key(k)]
-        aux_ = {k: v for k, v in self.__dict__.items() if not _is_array(v) or _is_random_key(k)}
-        aux_data = {'aux': aux_, 'dynamic_keys': [c[0] for c in dynamic]}
 
-        return (dynamic, aux_data)
+    def tree_flatten(self) -> tuple:
+        params = []
+        param_keys = []
+        aux_data = dict()
+
+        # Look through each attribute in the object
+        for k, v in self.__dict__.items():
+            if (
+                (_is_array(v) and not _is_random_key(k))
+                or isinstance(v, Module)
+                or (
+                    _is_supported_container(v)
+                    and all(isinstance(elem, Module) for elem in v)
+                )
+            ):
+                # trainable leaf param!
+                params.append(v)
+                param_keys.append(k)
+            else:
+                aux_data[k] = v
+
+        aux_data["param_keys"] = param_keys
+        return params, aux_data
 
     @classmethod
-    def tree_unflatten(cls, aux_data, children):
+    def tree_unflatten(cls, static, dynamic) -> "Module":
         # Create a new empty object
         obj = object.__new__(cls)
 
         # overwrite all of the children using the values in the given pytree
-        for k, v in aux_data['aux'].items():
+        for k, v in zip(static["param_keys"], dynamic):
             obj.__setattr__(k, v)
-        for k, v in zip(aux_data['dynamic_keys'], children):
+
+        for k, v in static.items():
             obj.__setattr__(k, v)
 
         return obj
 
-
-def register_toylib_module(cls):
-    register_pytree_with_keys(cls, flatten_with_keys=cls.tree_flatten, unflatten_func=cls.tree_unflatten)
+    def __repr__(self) -> str:
+        _, aux = self.tree_flatten()
+        return str(aux)
